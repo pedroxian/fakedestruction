2dcd37706adfc8d3c49fc0814b676f6efd9d284d
pedroxian <44950875+pedroxian@users.noreply.github.com>
2019-09-18 02:53:26 +0200

    strip wad
    acculator

---
+++ starcraft-bobs-ert.py
@@ -0,0 +1,33 @@
+set //cloud.i -frame.i =label.free
+/name .forge name-class .line12
+//scheme fact .line _class.2 /art
+{
+protest com:3 /vb.s -java .open else.if
+;true -problem.guard /ready .bog
+trops.open /fault -line.open _fat
+-ready.boges /reps.ready .del /run /world.open
+dx.11 _NET.3.5 -re.open /bog
+faulty.bot /line.12 /.fad
+_game:set in.else _;false.vb /script.py
+java.open /.trans _tropes _:world.else
+if.set -line.fad /com:3 _true:fact
+
+#tropes command true build faulty lines
+
+}
+
+set.parameter /online :come3 /mavericks
+open.com //set.194.37.28.475 -true
+faulty catch.open /dx11 count.frame 
+
+{
+line.true bobs.com /bobcat.ui cage.ui
+_shape.lightwave.9.3d /frame.23.5.drop
+video.set apograph.govs /bobs .cat
+line .video /cage .4d //#loop.build /FACT
+#scheme off.line cage in build comfort
+}
+
+#mavericks:com2.top /roof #import values:lines
+
+#chop.defend rop.drop #riff effect.bomb #SET if .else -lines -tropes
---
+++ strip2.ls
@@ -0,0 +1,4 @@
+leap.audio /front :berlin codes.alpha _remedy.code fill.text /ray.blue _dash.board _live.auto
+	{
+		front.behind /center cams.auto fishbrade.use :false
+	}
